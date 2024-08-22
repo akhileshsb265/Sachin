c9c8a5e76d978ab2d832085e6ffc09fb9b76f402
unknown <akhileshsb.xworkz@gmail.comgit config --global user.email akhileshsb.xworkz@gmail.com>
2024-08-22 19:47:53 +0530

    laptop

---
+++ Bank.java
@@ -0,0 +1,29 @@
+class Bank{
+	public static void main(String [] args){
+		
+		String branch="karnataka";
+		String area="kunigal";
+		int numOfStaff=5;
+	
+			if(branch=="sbi" && area=="mathikere"){
+				numOfStaff=3;
+				 System.out.println(numOfStaff);
+			}
+		    if(branch=="kotak" && area=="mathikere" ){
+				numOfStaff=7;
+			}
+			 if(branch=="karnataka" && area=="kunigal"){
+				 numOfStaff=5;
+			}
+			 if(branch=="sbi" && area=="yeshanthpur"){
+				 numOfStaff=1;
+				 System.out.println(numOfStaff);
+			 }
+			 if(branch=="sbi" && area=="mathikere"){
+				 numOfStaff=4;
+				 System.out.println(numOfStaff);
+				 
+							 
+	}
+	}
+}
---
+++ College.java
@@ -0,0 +1,8 @@
+class College {
+    public static void main(String[] args) {
+        int[] studentIDs = {3,4,5,6,7,8,9};
+        for (int i = 0; i < studentIDs.length; i++) {
+            System.out.println("Student ID: " + studentIDs[i]);
+        }
+    }
+}
