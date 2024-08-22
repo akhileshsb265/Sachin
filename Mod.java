b2948066977ecaf4b6adfe6b7bd95d4eee5c7548
unknown <akhileshsb.xworkz@gmail.comgit config --global user.email akhileshsb.xworkz@gmail.com>
2024-08-22 19:42:17 +0530

    not a good time

---
+++ Mod.java
@@ -0,0 +1,14 @@
+class Mod{
+	public static void main(String[]args){
+		System.out.println("Main method Starts");
+	    Mod();		
+		System.out.println("Main method ends");
+	}
+	public static void Mod(){
+		System.out.println("Hello");
+		sam();
+	}
+	public static void sam(){
+		System.out.println("bye");
+	}
+}
