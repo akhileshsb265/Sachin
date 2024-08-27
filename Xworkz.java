7daed89ab1d7d8ffb0f0deca0cd323b1e08af68d
unknown <akhileshsb.xworkz@gmail.comgit config --global user.email akhileshsb.xworkz@gmail.com>
2024-08-27 15:38:12 +0530

    task

---
+++ Xworkz.java
@@ -0,0 +1,11 @@
+class Xworkz{
+	public static int work(String manager){
+		return 55;
+	}
+	public static int management(float fees,int BatchNo,String StudName){
+		return 145;
+	}
+	public static int workers(int salary,String Lectueres){
+		return 42;
+	}
+}
---
+++ XworkzRunner.java
@@ -0,0 +1,7 @@
+class XworkzRunner{
+	public static void main(String []args){
+		System.out.println("Work list=="+Xworkz.work("price"));
+		System.out.println("Management list=="+Xworkz.management(358.2f,2, "Akhilesh"));
+		System.out.println("workers=="+Xworkz.workers(25000,"chethan"));
+	}
+}
